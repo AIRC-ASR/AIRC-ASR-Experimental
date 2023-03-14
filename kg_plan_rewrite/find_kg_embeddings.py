5fa3c6854369def29ebaa90a30d30ce32dcedb18
omarASC5 <ocobas19@avenues.org>
2023-03-14 11:35:41 -0400

    file update

---
+++ kg_plan_rewrite/find_kg_embeddings.py
@@ -0,0 +1,10 @@
+# TODO: Finish this function
+def text_to_kg_embedding(text):
+  '''This function takes a piece of text, creates a knowledge graph
+  from it based on the REBEL model and return knowledge graph embeddings.'''
+  # https://pykg2vec.readthedocs.io/en/latest/intro.html
+  # TODO: Check if the text is in a cache, and if so, return the knowledge graph
+  # TODO: embeddings from the cache, otherwise, store it in the cache
+  # TODO: Also store the exact model configurations
+  # TODO: Optionally, add tuning of the model here
+  pass
