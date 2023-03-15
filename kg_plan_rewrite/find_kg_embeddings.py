f9f4536ee0831d4444fd63efdc63b3cf5409f2c8
Omar Cobas <omarlcobas@gmail.com>
2023-03-15 10:28:01 -0400

    embeddings were found

--- kg_plan_rewrite/find_kg_embeddings.py
+++ kg_plan_rewrite/find_kg_embeddings.py
@@ -7,15 +7,20 @@ from torchkge.evaluation import LinkPredictionEvaluator
 from torchkge.data_structures import KnowledgeGraph
 from torchkge.models import TransEModel
 from torchkge.utils.datasets import load_fb15k
-from torchkge.utils import Trainer, MarginLoss
+from torchkge.utils import MarginLoss, DataLoader
+from tqdm.autonotebook import tqdm
+from torch import cuda
+
+from torchkge.sampling import BernoulliNegativeSampler
 
 
-with open("kg_plan_rewrite/redis_creds.json", "r") as file:
-    creds = json.loads(file.read())
-db = redis.Redis(**creds)
-db.set("foo", "bar")
-value = db.get('foo')
-print(value)
+
+# with open("kg_plan_rewrite/redis_creds.json", "r") as file:
+#     creds = json.loads(file.read())
+# db = redis.Redis(**creds)
+# db.set("foo", "bar")
+# value = db.get('foo')
+# print(value)
 
 
 def trained_embed(kg_train: KnowledgeGraph, kg_test: KnowledgeGraph, kg_val: KnowledgeGraph = None):
@@ -29,20 +34,46 @@ def trained_embed(kg_train: KnowledgeGraph, kg_test: KnowledgeGraph, kg_val: Kno
     # Load dataset
     # kg_train, kg_val, kg_test = load_fb15k()
 
-    # Define the model and criterion
+# Define the model and criterion
     model = TransEModel(emb_dim, kg_train.n_ent, kg_train.n_rel, dissimilarity_type='L2')
     criterion = MarginLoss(margin)
+
+    # Move everything to CUDA if available
+    if cuda.is_available():
+        cuda.empty_cache()
+        model.cuda()
+        criterion.cuda()
+
+    # Define the torch optimizer to be used
     optimizer = Adam(model.parameters(), lr=lr, weight_decay=1e-5)
 
-    trainer = Trainer(model, criterion, kg_train, n_epochs, batch_size,
-                      optimizer=optimizer, sampling_type='bern', use_cuda='all',)
+    sampler = BernoulliNegativeSampler(kg_train)
+    sampler.bern_probs = sampler.bern_probs.cuda()
+    dataloader = DataLoader(kg_train, batch_size=batch_size, use_cuda='all')
 
-    trainer.run()
+    iterator = tqdm(range(n_epochs), unit='epoch')
+    for epoch in iterator:
+        running_loss = 0.0
+        for i, batch in enumerate(dataloader):
+            h, t, r = batch[0], batch[1], batch[2]
+            n_h, n_t = sampler.corrupt_batch(h, t, r)
 
-    evaluator = LinkPredictionEvaluator(model, kg_test)
-    evaluator.evaluate(20)
-    evaluator.print_results()
+            optimizer.zero_grad()
+
+            # forward + backward + optimize
+            pos, neg = model(h, t, r, n_h, n_t)
+            loss = criterion(pos, neg)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+        iterator.set_description(
+            'Epoch {} | mean loss: {:.5f}'.format(epoch + 1,
+                                                running_loss / len(dataloader)))
+
+    model.normalize_parameters()
     return model
+
 
 
 # TODO: Finish this function
@@ -64,6 +95,7 @@ def text_to_kg_embedding(train_texts: list[str], test_texts: list[str]):
         
     
     dataset = [kb.to_torch_kg() for kb in dataset]
+    print('dataset', dataset)
 
     print("Training embeddings...")
     embed_model = trained_embed(*dataset)
