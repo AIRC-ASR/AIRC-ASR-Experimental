c9b7d8ff15d5b91c51051bc7621ff1c3213697db
omarASC5 <ocobas19@avenues.org>
2023-03-27 16:51:47 -0400

    added exp file

---
+++ bidirectional_predictions/TransformerCVAE/experiments/experiment2_test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+module load gcc cuda
+# sbatch -vvvv -N 1 --gres=gpu:1 -t 360 -p npl -o ./slurm_outputs/slurmoutput-%J.out ./experiments/experiment2.sh
+
+# 1 2 0 0
+python train_bidirectional.py \
+  test --add_input --learn_prior --fp16 --switch-time 0.5 \
+  --train_batch_size 2 --val_batch_size 2 --test_batch_size 2 \
+  --short_seq_len 2 --long_seq_len 2 \
+  --fwd_loss_weight 1 --bkwd_loss_weight 2 --all_sentence_loss_weight 0 \
+  --prompt_loss_weight 0
