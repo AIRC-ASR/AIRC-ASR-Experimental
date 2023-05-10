7106f9a5d6d6f207170e201413f57942a55b285a
matthew-pisano <matthewpisano14@gmail.com>
2023-05-10 14:47:02 -0400

    mistakeSummary

--- speechbrain/wav2vec2.py
+++ speechbrain/wav2vec2.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import numpy
 import sentencepiece
@@ -26,6 +27,20 @@ class CustomEncoder(EncoderASR):
             raise ValueError("filter_ctc_out can only filter python lists")
         return string_out
 
+    @classmethod
+    def merge(cls, scores: list[torch.Tensor], predictions: list[torch.Tensor], blank_id=-1, threshold: float = 3):
+        merged = [torch.clone(predictions[0])]
+        for i in range(len(predictions)-1):
+            merged.append(torch.clone(predictions[i]))
+            for j in range(len(merged[i])):
+                if abs(scores[i][j] - scores[i+1][j]) < threshold:
+                    # Choose next most likely choice if the difference of the scores are within the threshold
+                    merged[i+1][j] = predictions[i+1][j]
+
+            merged[i] = cls.filter_output(merged[i].tolist(), blank_id=blank_id)
+        merged[-1] = cls.filter_output(merged[-1].tolist(), blank_id=blank_id)
+
+        return merged
 
     @classmethod
     def decode_probs(cls, probabilities, seq_lens, blank_id=-1, top_k=1):
@@ -36,12 +51,14 @@ class CustomEncoder(EncoderASR):
         batch_outputs = []
         for seq, seq_len in zip(probabilities, seq_lens):
             actual_size = int(torch.round(seq_len * batch_max_len))
-            scores, predictions = torch.topk(seq.narrow(0, 0, actual_size), k=top_k, dim=1)
-            scores_split, predictions_split = torch.split(scores, 1, dim=1), torch.split(predictions, 1, dim=1)
-            outs = []
-            for i in range(len(predictions_split)):
-                outs.append(cls.filter_output(torch.squeeze(predictions_split[i]).tolist(), blank_id=blank_id))
-            batch_outputs.append(outs)
+            zipped_scores, zipped_predictions = torch.topk(seq.narrow(0, 0, actual_size), k=top_k, dim=1)
+            scores, predictions = list(torch.split(zipped_scores, 1, dim=1)), list(torch.split(zipped_predictions, 1, dim=1))
+
+            for i in range(len(predictions)):
+                predictions[i] = torch.squeeze(predictions[i])
+                scores[i] = torch.squeeze(scores[i])
+
+            batch_outputs.append(cls.merge(scores, predictions, blank_id=blank_id, threshold=3))
         return batch_outputs
 
     def transcribe_batch(self, wavs, wav_lens, top_k=1):
@@ -94,22 +111,118 @@ class CustomEncoder(EncoderASR):
         return predicted_words, predictions
 
 
-if __name__ == "__main__":
+# wavs, wav_lens, ref_text = zip(*[(x["speech"].numpy(), x["speech"].shape[0], x['text'].numpy().decode()) for idx, x in enumerate(ds[subset]) if idx<max_samples])
+
+def parse_mistakes(ref_text: str, hyp_text: str):
+    ref = ref_text.split()
+    hyp = hyp_text.split()
+
+    max_len = max(len(ref), len(hyp))
+    report = {"ins": [], "del": [], "sub": [], "total": 0, "words": len(ref)}
+
+    ref = ref + [None] * (max_len - len(ref))
+    hyp = hyp + [None] * (max_len - len(hyp))
+
+    hyp_offset = 0
+    for i in range(max_len):
+        if i+1+hyp_offset >= len(hyp):
+            hyp += [None]*(i+2+hyp_offset - len(hyp))
+
+        if ref[i] == hyp[i+hyp_offset]:
+            continue
+        report["total"] += 1
+        if i < max_len-1 and ref[i] == hyp[i+1+hyp_offset]:
+            report["ins"].append(i)
+            hyp_offset += 1
+        elif i < max_len-1 and ref[i+1] == hyp[i+hyp_offset]:
+            report["del"].append(i)
+            hyp_offset -= 1
+        else:
+            report["sub"].append(i)
+
+    return report, ref, hyp
+
+
+def summarize_reports(reports: list[dict], refs: list[str], hyp: list[str]):
+    summary = {"ins": 0, "del": 0, "sub": 0, "total": 0, "words": 0}
+
+    print("Mistakes:\nReference -> Hypothesis")
+    for i in range(len(reports)):
+        summary["ins"] += len(reports[i]["ins"])
+        summary["del"] += len(reports[i]["del"])
+        summary["sub"] += len(reports[i]["sub"])
+        summary["total"] += reports[i]["total"]
+        summary["words"] += reports[i]["words"]
+
+        for idx in reports[i]["ins"]:
+            print(f"Insertion: {refs[i][idx : idx+2]} -> {hyp[i][idx : idx+2]}")
+
+        for idx in reports[i]["del"]:
+            print(f"Deletion: {refs[i][idx : idx+2]} -> {hyp[i][idx : idx+2]}")
+
+        for idx in reports[i]["sub"]:
+            print(f"Substitution: {refs[i][idx : idx+2]} -> {hyp[i][idx : idx+2]}")
+
+    print("Total %WER {} [ {} / {}, {} ins, {} del, {} sub ]".format(round(summary["total"] / summary["words"]*100, 2),
+                                                                     summary["total"], summary["words"], summary['ins'], summary['del'], summary['sub']))
+
+
+def main():
+    PATTERN = """
+Reference:
+{ref}
+Hypothesis(es):
+{hyp}
+    ======================
+"""
+
+    subset = "dev_clean"
+
     asr_model = CustomEncoder.from_hparams(source="speechbrain/asr-wav2vec2-librispeech", savedir="pretrained_models/asr-wav2vec2-librispeech")
     # asr_model.transcribe_file("raw/3660-172183-0001.flac")
 
     ds = tfds.load('librispeech', builder_kwargs={'config': 'lazy_decode'})
+    ds_iter = iter(ds[subset])
 
-    subset = "dev_clean"
-    max_samples = 2
+    max_samples = 500
+    batch_size = 1
 
-    wavs, wav_lens = zip(*[(x["speech"].numpy(), x["speech"].shape[0]) for idx, x in enumerate(ds[subset]) if idx<max_samples])
-    max_len = max(wav_lens)
-    wav_lens = [[wav_len/max_len] for wav_len in wav_lens]
-    wavs = numpy.array([torch.nn.functional.pad(torch.Tensor(wav), (0, max_len-wav.shape[0]), mode="constant", value=0.0).numpy() for wav in wavs])
+    all_reports = []
+    all_refs = []
+    all_hyps = []
 
-    transcriptions = asr_model.transcribe_batch(torch.Tensor(wavs), torch.Tensor(wav_lens), top_k=2)
-    print(transcriptions)
+    for i in range(0, max_samples, batch_size):
+        wavs, wav_lens, ref_texts = [], [], []
+        print(f"\nLoading batch {i // batch_size}...\n")
+        for _ in range(batch_size):
+            sample = next(ds_iter)
+            wav, wav_len, ref_text = sample["speech"].numpy(), sample["speech"].shape[0], sample['text'].numpy().decode()
+            wavs.append(wav)
+            wav_lens.append(wav_len)
+            ref_texts.append(ref_text)
+
+        max_len = max(wav_lens)
+        wav_lens = [[wav_len / max_len] for wav_len in wav_lens]
+        wavs = numpy.array([torch.nn.functional.pad(torch.Tensor(wav), (0, max_len - wav.shape[0]),
+                                                    mode="constant", value=0.0).numpy() for wav in wavs])
+
+        transcriptions, tokens = asr_model.transcribe_batch(torch.Tensor(wavs), torch.Tensor(wav_lens), top_k=2)
+
+        for i, transcript in enumerate(transcriptions):
+            report, ref, hyp = parse_mistakes(ref_texts[i], transcript[0])
+            all_reports.append(report)
+            all_refs.append(ref)
+            all_hyps.append(hyp)
+
+            wer_str = "%WER {} [ {} / {}, {} ins, {} del, {} sub ]".format(round(report["total"] / report["words"]*100, 2),
+                                                                           report["total"], report["words"], len(report['ins']), len(report['del']), len(report['sub']))
+            print(wer_str)
+            print(PATTERN.format(ref=ref_texts[i], hyp=json.dumps(transcript).replace('", ', '\n').
+                                 replace("[", "").replace("]", "").replace('"', "")))
+
+    summarize_reports(all_reports, all_refs, all_hyps)
 
 
+if __name__ == "__main__":
 
+    main()
