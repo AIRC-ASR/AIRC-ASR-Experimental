5cb34398b81188abda4e83adc3cb211a89272497
Omar Cobas <omarlcobas@gmail.com>
2023-06-05 17:18:37 -0400

    added a sort to the suggested words by BERT based on levenshtein and phonetic distance

--- speechbrain/lm_homophones.py
+++ speechbrain/lm_homophones.py
@@ -1,44 +1,27 @@
-import nltk
+import Levenshtein
 import torch
-from nltk import ngrams
-from nltk.corpus import gutenberg
-from pyphones import Pyphones
-from transformers import AutoTokenizer, OPTForCausalLM
-from transformers import BertForMaskedLM, BertTokenizer
-
-nltk.download('gutenberg')
+import fuzzy
+from transformers import pipeline
 
 if __name__ == '__main__':
-  py = Pyphones("see")
-  candidate_words = py.get_the_homophones_as_list()
-  print('Candidate Words:', candidate_words)
-
-  # Step 1: Load BERT model and tokenizer
-  model_name = 'bert-large-uncased'
-  model = BertForMaskedLM.from_pretrained(model_name)
-  tokenizer = BertTokenizer.from_pretrained(model_name)
-
-  # Step 2: Tokenize sentence with [MASK] token
+  unmasker = pipeline('fill-mask', model='bert-base-uncased')
   sentence = "she could [MASK] the sound of waves crashing against the shore"
-  tokenized_sentence = tokenizer.tokenize(sentence)
-  masked_index = tokenized_sentence.index('[MASK]')
+  unmasker_output = unmasker(sentence)
+  predicted_words = [output['token_str'] for output in unmasker_output]
+  print('Predicted Words:', predicted_words)
 
-  # Step 3: Convert tokenized sentence to input features
-  indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_sentence)
-  segments_ids = [0] * len(indexed_tokens)
-  tokens_tensor = torch.tensor([indexed_tokens])
-  segments_tensor = torch.tensor([segments_ids])
+  replaced_word = 'tree'
+  levenshtein_distances = [Levenshtein.distance(replaced_word, word) for word in predicted_words]
+  fuzzy_distances = [Levenshtein.distance(fuzzy.nysiis(replaced_word), fuzzy.nysiis(word)) for word in predicted_words]
 
-  # Step 4: Pass input features through BERT model
-  model.eval()
-  with torch.no_grad():
-      outputs = model(tokens_tensor, segments_tensor)
-      predictions = outputs[0][0, masked_index]
+  levenshtein_weight = 0.6
+  phonetic_weight = 0.4
+  weighted_scores = [levenshtein_weight * levenshtein_distance + phonetic_weight * fuzzy_distance for levenshtein_distance, fuzzy_distance in zip(levenshtein_distances, fuzzy_distances)]
+  weights_and_words = list(zip(weighted_scores, predicted_words))
+  print('Weights and Words:', weights_and_words)
 
-  # Step 5: Retrieve predicted probabilities for each candidate word
-  predicted_probabilities = {word: predictions[tokenizer.convert_tokens_to_ids([word])[0]].item() for word in candidate_words}
+  sorted_words = [word for _, word in sorted(zip(weighted_scores, predicted_words))]
+  print('Sorted Words:', sorted_words)
 
-  # Step 6: Select word with highest probability
-  predicted_word = max(predicted_probabilities, key=predicted_probabilities.get)
-
-  print("Predicted missing word:", predicted_word)
+  best_word = sorted_words[0]
+  print('Best Word:', best_word)
